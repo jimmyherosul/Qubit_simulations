ad566317f38a51620c068fcfa1840436f03ff770
jimmyherosul <52802395+jimmyherosul@users.noreply.github.com>
2026-07-19 00:05:27 +0100

    Update print statement from 'Hello' to 'Goodbye'

--- 2.Driven_Two_level_system.py
+++ 2.Driven_Two_level_system.py
@@ -171,7 +171,6 @@ def gate_operation(initial_state, gate_sequence):
         # Generate tuning parameters (i.e. time duration, I/Q coupling signals and detuning signal) for the current gate.
         # The time points for each gate in tuning_parameters function always start from t = 0.
         gate_param = tuning_parameters(gate, hbar, Delta_eff, Delta, V_I_mag, V_Q_mag)
-
         t_current_gate = gate_param["t_points"]
 
         # Apply the corresponding unitary time-evolution operator to the state produced by the previous gate
